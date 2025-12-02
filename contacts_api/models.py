657553ea949eb2511d0800a60d6bcb8636635120
esegev1 <esegev1@gmail.com>
2025-12-02 17:18:47 -0500

    lab features work plus many to one level up

--- contacts_api/models.py
+++ contacts_api/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from companies_api.models import Company
 
 class Contact(models.Model):
     # Create your models here.
     name = models.CharField(max_length=32)
     age = models.IntegerField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
 
--- contacts_api/serializers.py
+++ contacts_api/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from .models import Contact
+from companies_api.serializers import CompanySerializer
 
 class ContactSerializer(serializers.ModelSerializer):
+    company = CompanySerializer()
     class Meta:
         model=Contact
-        fields=('id','name','age',)
+        fields=('id','name','age', 'company')
         
